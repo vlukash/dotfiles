0d459f83953fa558f08a07d35a14503568bd82f3
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2018-01-23 18:48:11 -0800

    Create symlinks, configure vim folders

--- install.ps1
+++ install.ps1
@@ -1,4 +1,7 @@
-﻿. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+﻿$homeDir = $env:USERPROFILE
+$dotfilesDir = $pwd.Path
+
+. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 
 Install-PackageProvider -Name NuGet -Force
 Import-PackageProvider NuGet -Force
@@ -13,8 +16,41 @@ Set-TaskbarOptions -Size Small -Lock -Combine Never
 # Enable-RemoteDesktop
 #netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
 
-install-module PsReadLine -Force
-PowerShellGet\Install-Module posh-git -Force
+#Install-module PsReadLine -Force
+Install-Module posh-git -Force
+Install-Module z -Force
 
 choco install vim -y
 choco install cmder -y
+choco install curl -y
+choco install python2 -y
+
+choco install ditto -y
+
+choco install keepass -y
+
+choco install notepadplusplus -y
+choco install visualstudiocode -y
+cinst kdiff3 -y
+
+cinst 7zip.install -y
+
+# Creating vim directories
+mkdir -p $homeDir\.vim-tmp -Force
+mkdir -p $homeDir\.vim\undodir -Force
+mkdir -p $homeDir\.vim\autoload -Force
+mkdir -p $homeDir\vimfiles\autoload -Force
+
+#Installing vim-plug
+& curl.exe -kfLo $homeDir\vimfiles\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# vimrc symlink
+New-Item -Path $home\_vimrc -ItemType SymbolicLink -Value $dotfilesDir\vim\_vimrc -Force
+
+# gitconfig symlink
+New-Item -Path $home\.gitconfig -ItemType SymbolicLink -Value $dotfilesDir\git\gitconfig.symlink -Force
+
+# ack symlink
+New-Item -Path $home\.tern-config -ItemType SymbolicLink -Value $dotfilesDir\tern\.tern-config -Force
+
+# Remap Caps
